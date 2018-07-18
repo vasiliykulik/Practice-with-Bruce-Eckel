d4551472623a79fcfa113d25b488fdd047e0a1c0
Raketa <detroi.vv@gmail.com>
2018-07-19 01:29:31 +0300

    Callable, Future

---
+++ src/ua/practice/java/ch16_Concurrency/CallableDemo.java
@@ -0,0 +1,42 @@
+package ch16_Concurrency;
+
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+/**
+ * @author Vasiliy Kylik on(Rocket) on 19.07.2018.
+ */
+
+class TaskWithResult implements Callable<String> {
+  private int id;
+
+  public TaskWithResult(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public String call() throws Exception {
+    return "result of TaskWithResult " + id;
+  }
+}
+
+public class CallableDemo {
+  public static void main(String[] args) {
+    ExecutorService exec = Executors.newCachedThreadPool();
+    ArrayList<Future<String>> results = new ArrayList<Future<String>>();
+    for (int i = 0; i < 10; i++)
+      results.add(exec.submit(new TaskWithResult(i)));
+    for (Future<String> fs : results)
+      try {
+        // get() blocks until completion:
+        System.out.println(fs.get());
+      } catch (InterruptedException e) {
+        System.out.println(e);
+        return;
+      } catch (ExecutionException e) {
+        System.out.println(e);
+      } finally {
+        exec.shutdown();
+      }
+  }
+}
