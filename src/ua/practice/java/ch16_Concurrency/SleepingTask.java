85f3920ed1c7765d0d5d72a3edc3060cd1eb1b79
Raketa <detroi.vv@gmail.com>
2018-07-20 01:47:19 +0300

    Sleeping, Priorities

--- src/ua/practice/java/ch16_Concurrency/SleepingTask.java
+++ src/ua/practice/java/ch16_Concurrency/SleepingTask.java
@@ -1,7 +1,29 @@
 package ch16_Concurrency;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Vasiliy Kylik on(Rocket) on 19.07.2018.
  */
-public class SleepingTask {
+public class SleepingTask extends LiftOff {
+  public static void main(String[] args) {
+    ExecutorService exec = Executors.newCachedThreadPool();
+    for (int i = 0; i < 5; i++) {
+      exec.execute(new SleepingTask());
+    }
+    exec.shutdown();
+  }
+
+  public void run() {
+    try {
+      while (countDown-- > 0) {
+        System.out.println(status());
+        TimeUnit.MILLISECONDS.sleep(1000);
+      }
+    } catch (InterruptedException e) {
+      System.err.println("Interrupted");
+    }
+  }
 }
