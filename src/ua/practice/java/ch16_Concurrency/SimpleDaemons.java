e71ed9a1e5c0f0dbcdc4f310a0b030437af2dc74
Raketa <detroi.vv@gmail.com>
2018-07-20 01:52:24 +0300

    Daemons

---
+++ src/ua/practice/java/ch16_Concurrency/SimpleDaemons.java
@@ -0,0 +1,11 @@
+package ch16_Concurrency;
+
+/**
+ * @author Vasiliy Kylik on(Rocket) on 20.07.2018.
+ */
+public class SimpleDaemons implements Runnable {
+  @Override
+  public void run() {
+
+  }
+}
