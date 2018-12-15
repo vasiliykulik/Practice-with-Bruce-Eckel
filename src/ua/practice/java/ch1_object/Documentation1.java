74fabd039c6c7f7adc71ff962437fc57856253e6
Raketa <detroi.vv@gmail.com>
2018-12-15 16:48:10 +0200

    Documentation1.java

--- src/ua/practice/java/ch1_object/Documentation1.java
+++ src/ua/practice/java/ch1_object/Documentation1.java
@@ -5,6 +5,23 @@ public class Documentation1 {
     public int i;
     /** A method comment*/
     public void f(){};
-
-
 }
+
+//: object/Documentation2.java
+/**
+ * <pre>
+ *     System.out.println(new Date());
+ * </pre>
+ */
+///:~
+
+//: object/Documentation3.java
+/**
+ * You can <em>even</em> insert a list:
+ * <ol>
+ * <li> Item one
+ * <li> Item two
+ * <li> Item three
+ * </ol>
+ */
+///:~
