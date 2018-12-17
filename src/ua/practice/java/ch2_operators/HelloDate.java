de821acae2e861bb48e2cb3de8cfee4d2f28972d
Vasiliy Kylik <detroit.vv@gmail.com>
2018-12-17 15:19:08 +0200

    operators

---
+++ src/ua/practice/java/ch2_operators/HelloDate.java
@@ -0,0 +1,14 @@
+package ch2_operators;
+import java.util.Date;
+
+import static util.Print.print;
+
+/**
+ * Created by Vasiliy Kylik (Lightning) on 17.12.2018.
+ */
+public class HelloDate {
+    public static void main(String[] args) {
+        print("Hello it's: ");
+        print(new Date());
+    }
+}
