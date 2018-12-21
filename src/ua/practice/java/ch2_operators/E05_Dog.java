09037822cce995c83dfab56f6cbb2d7853137292
Vasiliy Kylik <detroit.vv@gmail.com>
2018-12-21 23:50:06 +0200

    == vs equals

--- src/ua/practice/java/ch2_operators/E05_Dog.java
+++ src/ua/practice/java/ch2_operators/E05_Dog.java
@@ -3,13 +3,13 @@ package ch2_operators;
 /**
  * Created by Vasiliy Kylik (Lightning) on 20.12.2018.
  */
-public class Dog {
+public class E05_Dog {
     String name;
     String says;
 
     public static void main(String[] args) {
-        Dog dog1 = new Dog();
-        Dog dog2 = new Dog();
+        E05_Dog dog1 = new E05_Dog();
+        E05_Dog dog2 = new E05_Dog();
         dog1.name = "spot";
         dog1.says = "Ruff!";
         dog2.name = "scruffy";
--- src/ua/practice/java/ch2_operators/E06_DogsComparison.java
+++ src/ua/practice/java/ch2_operators/E06_DogsComparison.java
@@ -5,13 +5,32 @@ import static util.Print.print;
  * Created by Vasiliy Kylik (Lightning) on 21.12.2018.
  */
 /****************** Exercise 6 *****************
- * Following Exercise 5 assign, a new Dog
+ * Following Exercise 5 assign, a new E05_Dog
  * reference to spot's object. Test for comparison
  * using == and equals() for all references.
  ***********************************************/
 public class E06_DogsComparison {
-    static void compare(Dog dog1, Dog dog2){
+    static void compare(E05_Dog dog1, E05_Dog dog2){
         print("== on top references: " + (dog1==dog2));
-        print("equals() on top references")
+        print("equals() on top references" + dog1.equals(dog2));
+
+        print("== on top names: " + (dog1.name==dog2.name));
+        print("equals() on top names" + dog1.name.equals(dog2.name));
+
+        print("== on top says: " + (dog1.says==dog2.says));
+        print("equals() on top says" + dog1.says.equals(dog2.says));
+    }
+
+    public static void main(String[] args) {
+        E05_Dog dog1 = new E05_Dog();
+        E05_Dog dog2 = new E05_Dog();
+        E05_Dog dog3 = dog1; // Aliased reference
+        dog1.name = "spot";
+        dog1.says = "Ruff!";
+        dog2.name = "scruffy";
+        dog2.says = "Wurf!";
+        System.out.println(dog1.name+ " : "+dog1.says);
+        System.out.println(dog2.name+ " : "+dog2.says);
+
     }
 }
