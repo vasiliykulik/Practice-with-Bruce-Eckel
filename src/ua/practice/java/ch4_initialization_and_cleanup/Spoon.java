b5a5b8470ba5aef08d5d28a43387fa7ccd5e6be0
Raketa <detroi.vv@gmail.com>
2019-07-24 22:28:38 +0300

    ExplicitStatic

---
+++ src/ua/practice/java/ch4_initialization_and_cleanup/Spoon.java
@@ -0,0 +1,11 @@
+package ch4_initialization_and_cleanup;
+
+/**
+ * @author Vasiliy Kylik on(Rocket) on 10.07.2019.
+ */
+public class Spoon {
+    static int i;
+    static {
+        i = 47;
+    }
+}
