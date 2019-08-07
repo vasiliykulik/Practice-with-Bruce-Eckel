5eb05ee05c3a22a13b5d4f02751570e8aa40140d
Vasiliy Kylik <detroit.vv@gmail.com>
2019-08-07 15:12:36 +0300

    E15_StringInstanceInitialization

---
+++ src/ua/practice/java/ch4_initialization_and_cleanup/E13_LeftToReader.java
@@ -0,0 +1,18 @@
+package ch4_initialization_and_cleanup;
+
+/****************** Exercise 13 *****************
+ * Comment the line marked (1) in
+ * ExplicitStatic.java and verify that the static
+ * initialization clause is not called. Now
+ * uncomment one of the lines marked (2) and
+ * verify that the static initialization clause
+ * is called. Finally, uncomment the other line marked
+ * (2) and verify that static initialization
+ * occurs only once.
+ ************************************************/
+
+public class E13_LeftToReader {
+    public static void main(String args[]) {
+        System.out.println("Exercise left to reader");
+    }
+}
